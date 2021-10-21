9cb7b6bd202de229f509e1223d59617ca348c454
SebastienDS <sebastien.dossantos.77@gmail.com>
2021-10-21 16:05:17 +0200

    draw a rect

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,4 @@
-import java.awt.EventQueue;
+import java.awt.*;
 import javax.swing.JFrame;
 
 public class Application extends JFrame {
@@ -8,13 +8,14 @@ public class Application extends JFrame {
     }
 
     private void initUI() {
-        add(new Game());
-
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 //        setUndecorated(true);
 
         setTitle("Sus Fighter");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        var dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        add(new Game(dimension.getWidth(), dimension.getHeight()));
     }
 
     public static void main(String[] args) {
--- src/Game.java
+++ src/Game.java
@@ -5,15 +5,24 @@ import java.awt.event.KeyEvent;
 
 public class Game extends JPanel {
 
-    public Game() {
+    private int x = 0;
+    private int y = 0;
+    private int size = 50;
+
+    public Game(double width, double height) {
         setFocusable(true);
         addKeyListener(new TAdapter());
+
+        this.x = (int)width / 2 - size / 2;
+        this.y = (int)height / 2 - size / 2;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.setColor(Color.PINK);
+        g.fillRect(x, y, size, size);
     }
 
     private class TAdapter extends KeyAdapter {
